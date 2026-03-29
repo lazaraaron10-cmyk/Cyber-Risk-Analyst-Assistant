09893030a6daa1cb461704b2045ef8247d4723ff
Aaron Lazar <lazaraaron10@gmail.com>
2026-03-30 04:01:20 +0530

    Refactor app.py to use environment variables

--- template/app.py
+++ template/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify, render_template
 from groq import Groq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__, template_folder='.')
 
-GROQ_API_KEY = "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"
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 client = Groq(api_key=GROQ_API_KEY)
 
